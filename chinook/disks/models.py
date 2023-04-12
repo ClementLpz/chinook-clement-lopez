3f63d7fc524e9779d1d99bec780d69eb959e04b7
Clement Lopez <clement.lopez@student.isae-supaero.fr>
2023-04-12 11:14:26 +0200

    Creation of the database

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Track(models.Model):
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField(default=0)
+    bytes = models.IntegerField(default=0)
+    unitPrice = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.question_text
